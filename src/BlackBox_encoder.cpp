57440e1afd16c2c628024bd2e5ea38e5329474c2
haberturdeur <tomas.rohlinek@outlook.cz>
2020-06-19 16:58:19 +0200

    Switch to another system for encoder
    The new system uses hardware pcnt rather than periodicaly reading gpios
    Also removed dependencies of the old system

--- src/BlackBox_encoder.cpp
+++ src/BlackBox_encoder.cpp
@@ -1,63 +1,123 @@
 #include "BlackBox_encoder.hpp"
 
 namespace BlackBox {
-void BlackBox_encoder::init()
-{
-    m_eventQueue = xQueueCreate(EV_QUEUE_LEN, sizeof(rotary_encoder_event_t));
-    ESP_ERROR_CHECK(rotary_encoder_init(m_eventQueue));
-    memset(&m_encoder, 0, sizeof(rotary_encoder_t));
-    m_encoder.pin_a = BlackBox::ENC_A_GPIO;
-    m_encoder.pin_b = BlackBox::ENC_B_GPIO;
-    m_encoder.pin_btn = BlackBox::ENC_SW_GPIO;
-    ESP_ERROR_CHECK(rotary_encoder_add(&m_encoder));
+
+void BlackBox_encoder::initEncoder() {
+    ESP_LOGD(m_tag, "Initializing encoder.");
+    ESP_LOGI(m_tag, "Locking mutex.");
+    std::lock_guard<std::recursive_mutex> encoderLock(m_mutex);
+    ESP_ERROR_CHECK(pcnt_unit_config(&pcntConfig));
+
+    ESP_ERROR_CHECK(pcnt_set_filter_value(PCNT_UNIT, 100));
+    ESP_ERROR_CHECK(pcnt_filter_enable(PCNT_UNIT));
+
+    /* Set threshold 0 and 1 values and enable events to watch */
+    // pcnt_set_event_value(PCNT_UNIT, PCNT_EVT_THRES_1, PCNT_THRESH1_VAL);
+    // pcnt_event_enable(PCNT_UNIT, PCNT_EVT_THRES_1);
+    // pcnt_set_event_value(PCNT_UNIT, PCNT_EVT_THRES_0, PCNT_THRESH0_VAL);
+    // pcnt_event_enable(PCNT_UNIT, PCNT_EVT_THRES_0);
+    // /* Enable events on zero, maximum and minimum limit values */
+    // pcnt_event_enable(PCNT_UNIT, PCNT_EVT_ZERO);
+    // pcnt_event_enable(PCNT_UNIT, PCNT_EVT_H_LIM);
+    // pcnt_event_enable(PCNT_UNIT, PCNT_EVT_L_LIM);
+
+    ESP_ERROR_CHECK(pcnt_counter_pause(PCNT_UNIT));
+    ESP_ERROR_CHECK(pcnt_counter_clear(PCNT_UNIT));
+
+    ESP_ERROR_CHECK(pcnt_counter_resume(PCNT_UNIT));
 }
 
-void BlackBox_encoder::act()
-{
-    xQueueReceive(m_eventQueue, &m_lastEvent, portMAX_DELAY);
-    switch (m_lastEvent.type) {
-    case RE_ET_BTN_PRESSED:
-        m_onPress(m_lastEvent);
-        break;
-    case RE_ET_BTN_RELEASED:
-        m_onRelease(m_lastEvent);
-        break;
-    case RE_ET_BTN_CLICKED:
-        m_onClick(m_lastEvent);
-        break;
-    case RE_ET_BTN_LONG_PRESSED:
-        m_onLongPress(m_lastEvent);
-        break;
-    case RE_ET_CHANGED:
-        m_onValueChange(m_lastEvent);
-        break;
+void BlackBox_encoder::initButton() {
+    ESP_LOGD(m_tag, "Initializing button.");
+    ESP_LOGI(m_tag, "Locking mutex.");
+    std::lock_guard<std::recursive_mutex> lock(m_mutex);
+    ESP_ERROR_CHECK(gpio_config(&gpioConfig));
+}
+
+void BlackBox_encoder::init() {
+    initEncoder();
+    initButton();
+}
+
+esp_err_t BlackBox_encoder::readEncoder() {
+    ESP_LOGD(m_tag, "Reading encoder.");
+    ESP_LOGI(m_tag, "Locking mutex.");
+    std::lock_guard<std::recursive_mutex> lock(m_mutex);
+    int16_t count = 0;
+    esp_err_t err = pcnt_get_counter_value(PCNT_UNIT, &count);
+    uint8_t value = (count >= 0) ? count : (60 + count);
+    if (m_value != value) {
+        m_value = value;
     }
+    return err;
 }
 
-void BlackBox_encoder::setFunction(rotary_encoder_event_type_t event, encoder_function_t function)
-{
-    switch (event) {
-    case RE_ET_BTN_PRESSED:
-        m_onPress = function;
-        break;
-    case RE_ET_BTN_RELEASED:
-        m_onRelease = function;
-        break;
-    case RE_ET_BTN_CLICKED:
-        m_onClick = function;
-        break;
-    case RE_ET_BTN_LONG_PRESSED:
-        m_onLongPress = function;
-        break;
-    case RE_ET_CHANGED:
-        m_onValueChange = function;
-        break;
+void BlackBox_encoder::readButton() {
+    ESP_LOGD(m_tag, "Reading button.");
+    ESP_LOGI(m_tag, "Locking mutex.");
+    std::lock_guard<std::recursive_mutex> lock(m_mutex);
+    uint32_t now = esp_log_timestamp();
+    uint32_t elapsed = now - m_lastCheck;
+    m_rawButtonState = !gpio_get_level(ENC_SW_GPIO);
     }
+    //FIXME: Write function for reading button.
+    m_lastCheck = now;
 }
 
-rotary_encoder_event_t& BlackBox_encoder::receiveNewEvent()
-{
-    xQueueReceive(m_eventQueue, &m_lastEvent, portMAX_DELAY);
-    return lastEvent();
+esp_err_t BlackBox_encoder::read() {
+    readButton();
+    return readEncoder();
 }
+
+}
+
+}
+
+index_t BlackBox_encoder::valueIndex(bool i_renew) {
+    if (i_renew)
+        ESP_ERROR_CHECK(readEncoder());
+    ESP_LOGI(m_tag, "Locking mutex.");
+    std::lock_guard<std::recursive_mutex> lock(m_mutex);
+    return index_t(m_value);
+}
+
+uint8_t BlackBox_encoder::value(bool i_renew) {
+    if (i_renew)
+        ESP_ERROR_CHECK(readEncoder());
+    ESP_LOGI(m_tag, "Locking mutex.");
+    std::lock_guard<std::recursive_mutex> lock(m_mutex);
+    return m_value;
+}
+
+button_state_t BlackBox_encoder::buttonState(bool i_renew) {
+    if (i_renew)
+        readButton();
+    ESP_LOGI(m_tag, "Locking mutex.");
+    std::lock_guard<std::recursive_mutex> lock(m_mutex);
+    return m_buttonState;
+}
+void BlackBox_encoder::setReleaseTimeout(uint32_t i_releaseTimeout) {
+    ESP_LOGI(m_tag, "Locking mutex.");
+    std::lock_guard<std::recursive_mutex> lock(m_mutex);
+    m_releaseTimeout = i_releaseTimeout;
+}
+
+void BlackBox_encoder::setDebounceTime(uint32_t i_debounceTime) {
+    ESP_LOGI(m_tag, "Locking mutex.");
+    std::lock_guard<std::recursive_mutex> lock(m_mutex);
+    m_debounceTime = i_debounceTime;
+}
+
+void BlackBox_encoder::setLongClickTimeout(uint32_t i_longClickTimeout) {
+    ESP_LOGI(m_tag, "Locking mutex.");
+    std::lock_guard<std::recursive_mutex> lock(m_mutex);
+    m_longClickTimeout = i_longClickTimeout;
+}
+
+void BlackBox_encoder::setDoubleClickTimeout(uint32_t i_doubleClickTimeout) {
+    ESP_LOGI(m_tag, "Locking mutex.");
+    std::lock_guard<std::recursive_mutex> lock(m_mutex);
+    m_doubleClickTimeout = i_doubleClickTimeout;
+}
+
 } // namespace BlackBox
--- src/BlackBox_encoder.hpp
+++ src/BlackBox_encoder.hpp
@@ -1,49 +1,106 @@
 #pragma once
 
-#include <cstdint>
-#include <encoder.h>
-#include <esp_idf_lib_helpers.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
+#include <cassert>
 #include <functional>
-#include <string.h>
+#include <memory>
+#include <mutex>
 
+#include "esp_log.h"
+#include <driver/gpio.h>
+#include <driver/pcnt.h>
+
+
+#include "BlackBox_LEDring.hpp"
 #include "BlackBox_pinout.hpp"
 
-#define EV_QUEUE_LEN 5
+#define DEFAULT_RELEASE_TIMEOUT 10000000
+#define DEFAULT_DEBOUNCE_TIME 10000
+#define DEFAULT_LONG_CLICK_TIMEOUT 500000
+#define DEFAULT_DOUBLE_CLICK_TIMEOUT 500000
+
+#define PCNT_UNIT PCNT_UNIT_0
+#define PCNT_H_LIM_VAL 60
+#define PCNT_L_LIM_VAL -60
+
+#define SW_GPIO_SEL 1ULL << BlackBox::ENC_SW_GPIO
 
 namespace BlackBox {
 
-typedef std::function<void(rotary_encoder_event_t)> encoder_function_t;
+typedef enum {
+    BUTTON_RELEASED = 0,
+    BUTTON_PRESSED = 1,
+    BUTTON_LONGPRESSED = 2,
+} button_state_t;
+
+static const pcnt_config_t pcntConfig = {
+    // Set PCNT input signal and control GPIOs
+    .pulse_gpio_num = BlackBox::ENC_A_GPIO,
+    .ctrl_gpio_num = BlackBox::ENC_B_GPIO,
+    // What to do when control input is low or high?
+    .lctrl_mode = PCNT_MODE_REVERSE, // Reverse counting direction if low
+    .hctrl_mode = PCNT_MODE_KEEP, // Keep the primary counter mode if high
+    // What to do on the positive / negative edge of pulse input?
+    .pos_mode = PCNT_COUNT_INC, // Count up on the positive edge
+    .neg_mode = PCNT_COUNT_DIS, // Keep the counter value on the negative edge
+    // Set the maximum and minimum limit values to watch
+    .counter_h_lim = PCNT_H_LIM_VAL,
+    .counter_l_lim = PCNT_L_LIM_VAL,
+    .unit = PCNT_UNIT,
+    .channel = PCNT_CHANNEL_0,
+};
+
+static const gpio_config_t gpioConfig = {
+    .pin_bit_mask = SW_GPIO_SEL,
+    .mode = GPIO_MODE_INPUT,
+    .pull_up_en = GPIO_PULLUP_ENABLE,
+    .pull_down_en = GPIO_PULLDOWN_DISABLE,
+    .intr_type = GPIO_INTR_DISABLE,
+};
 
 class BlackBox_encoder {
     friend class BlackBox_interface;
 
 private:
     BlackBox_encoder() = default;
+    BlackBox_encoder(uint32_t i_releaseTimeout, uint32_t m_debounceTime, uint32_t i_longClickTimeout, uint32_t i_doubleClickTimeout);
 
-    QueueHandle_t m_eventQueue;
-    rotary_encoder_t m_encoder;
-    rotary_encoder_event_t m_lastEvent;
+    const char* m_tag = "BB_encoder";
 
-    encoder_function_t m_onPress = [](rotary_encoder_event_t e) { printf("Button pressed\n"); };
-    encoder_function_t m_onRelease = [](rotary_encoder_event_t e) { printf("Button released\n"); };
-    encoder_function_t m_onClick = [](rotary_encoder_event_t e) { printf("Button clicked\n"); };
-    encoder_function_t m_onLongPress = [](rotary_encoder_event_t e) { printf("Button long pressed\n"); };
-    encoder_function_t m_onValueChange = [](rotary_encoder_event_t e) { printf("Encoder value change by %d\n", e.diff); };
+    uint32_t m_releaseTimeout = DEFAULT_RELEASE_TIMEOUT; // 10 s
+    uint32_t m_debounceTime = DEFAULT_DEBOUNCE_TIME; // 10 ms
+    uint32_t m_longClickTimeout = DEFAULT_LONG_CLICK_TIMEOUT; // 500 ms
+    uint32_t m_doubleClickTimeout = DEFAULT_DOUBLE_CLICK_TIMEOUT; // 500 ms
+
+
+
+    void initEncoder();
+    void initButton();
+    void initFlags(uint8_t i_flags);
+    // void init();
+
+    esp_err_t readEncoder();
+    void readButton();
+    // esp_err_t read();
+
 
 public:
-    void init();
-    void act();
+    index_t valueIndex(bool i_renew = 0);
+    uint8_t value(bool i_renew = 0);
+    button_state_t buttonState(bool i_renew = 0);
+    // bool awaitRelease(uint32_t i_releaseTimeout = m_releaseTimeout);
 
-    rotary_encoder_event_t& lastEvent() { return m_lastEvent; }
-    rotary_encoder_event_t& receiveNewEvent();
 
-    QueueHandle_t& getQueueHandle() { return m_eventQueue; };
+    void setReleaseTimeout(uint32_t i_releaseTimeout);
+    void setDebounceTime(uint32_t i_debounceTime);
+    void setLongClickTimeout(uint32_t i_longClickTimeout);
+    void setDoubleClickTimeout(uint32_t i_doubleClickTimeout);
 
-    void setFunction(rotary_encoder_event_type_t event, encoder_function_t function);
+
+    std::recursive_mutex& mutex() { return m_mutex; }
 
     ~BlackBox_encoder() = default;
+
+    const char* tag() { return m_tag; }
 };
 
 } // namespace BlackBox
