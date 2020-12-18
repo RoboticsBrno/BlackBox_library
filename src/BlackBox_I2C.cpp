6904c540bed31d0ab0017d32105cab8d6b096137
Tomáš Rohlínek <tomas.rohlinek@outlook.cz>
2020-12-18 23:46:03 +0100

    Add implementation of i2c transmission

---
+++ src/BlackBox_I2C.cpp
@@ -0,0 +1,76 @@
+#include "BlackBox_I2C.hpp"
+
+#include "esp_log.h"
+
+#include <mutex>
+
+namespace BlackBox {
+I2CTransmission::I2CTransmission()
+    : m_handle(i2c_cmd_link_create()) {
+}
+
+I2CTransmission::I2CTransmission(i2c_cmd_handle_t const i_handle)
+    : m_handle(i_handle) {
+}
+
+I2CTransmission::~I2CTransmission() {
+    std::scoped_lock lock(m_mutex);
+    i2c_cmd_link_delete(m_handle);
+    m_handle = NULL;
+}
+
+esp_err_t I2CTransmission::startBit() {
+    std::scoped_lock lock(m_mutex);
+    return i2c_master_start(m_handle);
+}
+
+esp_err_t I2CTransmission::stopBit() {
+    std::scoped_lock lock(m_mutex);
+    return i2c_master_stop(m_handle);
+}
+
+esp_err_t I2CTransmission::writeByte(uint8_t i_data, i2c_ack_type_t i_ackType) {
+    std::scoped_lock lock(m_mutex);
+    return i2c_master_write_byte(m_handle, i_data, i_ackType);
+}
+
+esp_err_t I2CTransmission::readByte(uint8_t* o_data, i2c_ack_type_t i_ackType) {
+    return i2c_master_read_byte(m_handle, o_data, i_ackType);
+}
+
+esp_err_t I2CTransmission::write(uint8_t* i_data, size_t i_dataLengt, i2c_ack_type_t i_ackType) {
+    std::scoped_lock lock(m_mutex);
+    return i2c_master_write(m_handle, i_data, i_dataLengt, i_ackType);
+}
+
+esp_err_t I2CTransmission::read(uint8_t* o_data, size_t i_dataLengt, i2c_ack_type_t i_ackType) {
+    std::scoped_lock lock(m_mutex);
+    return i2c_master_read(m_handle, o_data, i_dataLengt, i_ackType);
+}
+
+esp_err_t I2CTransmission::send(i2c_port_t i_i2cNum, TickType_t i_ticksToWait) {
+    std::scoped_lock lock(m_mutex);
+    m_sent = true;
+    return i2c_master_cmd_begin(i_i2cNum, m_handle, i_ticksToWait);
+}
+
+void I2CTransmission::reset() {
+    std::scoped_lock lock(m_mutex);
+    if (!m_sent)
+        ESP_LOGW(m_tag, "Reseting without sending data!");
+    i2c_cmd_link_delete(m_handle);
+    m_handle = i2c_cmd_link_create();
+    m_sent = false;
+}
+
+i2c_cmd_handle_t I2CTransmission::raw() {
+    std::scoped_lock lock(m_mutex);
+    return m_handle;
+}
+
+void I2CTransmission::detach() {
+    std::scoped_lock lock(m_mutex);
+    m_handle = NULL;
+}
+
+}
--- src/BlackBox_I2C.hpp
+++ src/BlackBox_I2C.hpp
@@ -9,26 +9,33 @@ private:
     I2CTransmission(const I2CTransmission&) = delete;
     I2CTransmission& operator=(const I2CTransmission&) = delete;
 
+    mutable std::recursive_mutex m_mutex;
+    const char* m_tag = "I2C_Transmission";
+
     i2c_cmd_handle_t m_handle;
-    std::recursive_mutex m_mutex;
-    bool m_sent;
+    bool m_sent = false;
 
 public:
     I2CTransmission();
-    I2CTransmission(i2c_cmd_handle_t* const);
+    I2CTransmission(i2c_cmd_handle_t const);
     I2CTransmission(I2CTransmission&&) = default;
     I2CTransmission& operator=(I2CTransmission&&) = default;
 
     ~I2CTransmission();
 
-    esp_err_t start(uint8_t i_address);
-    esp_err_t write(const uint8_t* const i_data, size_t i_dataLengt, i2c_ack_type_t i_ackType);
-    esp_err_t read(const uint8_t* const i_data, size_t i_dataLengt, i2c_ack_type_t i_ackType);
-    esp_err_t stop();
-    esp_err_t send(bool i_noStopBit = 0);
-    void reset(bool i_send = 0);
+    esp_err_t startBit();
+    esp_err_t stopBit();
 
-    i2c_cmd_handle_t* raw();
+    esp_err_t writeByte(uint8_t data, i2c_ack_type_t ackType = I2C_MASTER_ACK);
+    esp_err_t readByte(uint8_t* data, i2c_ack_type_t ackType = I2C_MASTER_ACK);
+
+    esp_err_t write(uint8_t* data, size_t dataLength, i2c_ack_type_t ackType = I2C_MASTER_ACK);
+    esp_err_t read(uint8_t* data, size_t dataLength, i2c_ack_type_t ackType = I2C_MASTER_ACK);
+
+    esp_err_t send(i2c_port_t i2cNum, TickType_t ticksToWait);
+
+    void reset();
+    i2c_cmd_handle_t raw();
     void detach();
 };
 }
