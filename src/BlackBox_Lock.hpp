23f733c7fe03b7439034e5467a62b939985724f9
Tomáš Rohlínek <tomas.rohlinek@outlook.cz>
2021-02-25 11:24:10 +0100

    Add documentation for Lock

--- src/BlackBox_Lock.hpp
+++ src/BlackBox_Lock.hpp
@@ -28,18 +28,42 @@ private:
 public:
     ~Lock() = default;
 
+    /**
+     * @brief Construct a new Lock object
+     * 
+     * @param motor pin on which motor is connected
+     * @param encoderA pin on which encoder is connected
+     * @param encoderB pin on which encoder is connected
+     * @param timer number of timer to use (for compability with other applications)
+     * @param channel number of LEDC channel to use (for compability with other applications)
+     */
     Lock(gpio_num_t motor = Pins::Lock::Motor,
         gpio_num_t encoderA = Pins::Lock::A,
         gpio_num_t encoderB = Pins::Lock::B,
         ledc_timer_t timer = LEDC_TIMER_0,
         ledc_channel_t channel = LEDC_CHANNEL_0);
 
+    /**
+     * @brief Initialize lock
+     */
     void init();
 
+    /**
+     * @brief Lock lock
+     */
     void lock();
 
+    /**
+     * @brief Unlock lock
+     */
     void unlock();
 
+    /**
+     * @brief Whether or not the lock is locked
+     * 
+     * @retval true - locked
+     * @retval false - unlocked
+     */
     bool locked();
 };
 } // namespace BlackBox
