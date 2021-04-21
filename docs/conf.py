00002db822dd9f14fb1689a215ae549d574597aa
haberturdeur <tomas.rohlinek@outlook.cz>
2021-04-21 23:57:14 +0200

    Start adding more languages to docs

--- docs/conf.py
+++ docs/conf.py
@@ -195,11 +195,6 @@ epub_exclude_files = ['search.html']
 
 # -- Extension configuration -------------------------------------------------
 
-# -- Options for intersphinx extension ---------------------------------------
-
-# Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
-
 # -- Options for todo extension ----------------------------------------------
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
