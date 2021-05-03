33797be3e948e8536d2e1617c7964ec94a6d1657
Tomáš Rohlínek <tomas.rohlinek@outlook.cz>
2021-05-03 10:14:26 +0200

    Finish adding support for hardware v1.1

--- include/library/BlackBox_pinout.hpp
+++ include/library/BlackBox_pinout.hpp
@@ -7,7 +7,7 @@
 
 #pragma once
 
-#if BB_HW_VER == 0x0100
+#if (BB_HW_VER == 0x0100) || (BB_HW_VER == 0x0101) // nothing changed between v1.0 and v1.1
 #include "library/pinout/v1_0.hpp"
 #else
 #error "Invalid BB_HW_VER"
