c58ed713504ea62a752359adb0f95bbc2b1af1dd
Tomáš Rohlínek <tomas.rohlinek@outlook.cz>
2021-05-11 18:38:50 +0200

    Update examples

--- examples/ring/main.cpp
+++ examples/ring/main.cpp
@@ -7,25 +7,26 @@ using namespace BlackBox;
 extern "C" {
 
 void app_main() {
-    Manager& man = Manager::singleton();
+    Manager& man = Manager::singleton(); // Get instance of Manager class
 
-    man.init();
+    man.init(); // Initialize BlackBox's peripherials
 
-    man.power().turnOn5V();
+    man.power().turnOn5V(); // Turn on 5V power, meaning LEDs and lock
 
-    Ring& ring = man.ring();
+    Ring& ring = man.ring(); // Get instance of Ring class
 
-    ring.clear();
-    ring.setDarkModeValue(100);
-    ring.enableDarkMode();
+    ring.clear(); // Clear display(Ring)
+    ring.setDarkModeValue(50); // Set maximum value for darkmode
+    ring.enableDarkMode(); // Enable dark mode
 
     
     while (true) {
-        ring.drawArc(Rgb(255,0,0), 0, 37, ArcType::Clockwise);
+        // Draw arcs with given parameters. See documentation for more information 
+        ring.drawArc(Rgb(255,0,0), 0, 37, ArcType::Clockwise); // Draw red arc from Index 0 to(including) 37 going Clockwise
         ring.drawArc(Rgb(0, 255,0), 0, 26);
         ring.drawArc(Rgb(0,0, 255), 0, 15);
 
-        ring.show();
+        ring.show(); // Show changes on display
 
         vTaskDelay(10 / portTICK_PERIOD_MS);
     }
--- examples/touchpad/main.cpp
+++ examples/touchpad/main.cpp
@@ -1,47 +1,46 @@
-#include "library/BlackBox_Manager.hpp"
+#include "library/BlackBox_Manager.hpp" // Main BlackBox library header file
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-using namespace BlackBox;
+using namespace BlackBox; // For convenience use whole namespace
 
 extern "C" {
 
 void app_main() {
-    Manager& man = Manager::singleton();
+    Manager& man = Manager::singleton(); // Get instance of Manager class
 
-    man.init();
+    man.init(); // Initialize BlackBox's peripherials
 
-    man.power().turnOn5V();
+    man.power().turnOn5V(); // Turn on 5V power, meaning LEDs and lock
 
-    Ring& ring = man.ring();
+    Ring& ring = man.ring(); // Get instance of Ring class
 
-    ring.clear();
-    ring.setDarkModeValue(50);
-    ring.enableDarkMode();
+    ring.clear(); // Clear display(Ring)
+    ring.setDarkModeValue(50); // Set maximum value for darkmode
+    ring.enableDarkMode(); // Enable dark mode
 
-    float a[4] = { 1, 1, 1, 1 };
+    Touchpad& touchpad = man.touchpad(); // Get instance of Touchpad class
 
-    Touchpad touchpad(16, 0b1110, a);
+    // Optional setup for better calibration of LDC
 
+    /* 
     auto regs = man.ldc().registers();
-
     for (int i = 0; i < 4; i++) {
         regs.settleCount[i] = 10;
         regs.referenceCount[i] = 1238;
         regs.driveCurrent[i].reg = 0x9000;
     }
-
     man.ldc().writeRegisters(regs);
+    */
 
     while (true) {
-        BlackBox::Coords data;
-        BlackBox::Coords out = man.touchpad().calculate();;
-        ring.clear();
-        vTaskDelay(10 / portTICK_PERIOD_MS);
-        ring[out] = Rgb(255, 0, 0);
-        ring.show();
+        Coords out = man.touchpad().calculate(); // Calculate coordinates of touch
+        ring.clear(); // Clear display
+        vTaskDelay(10 / portTICK_PERIOD_MS); // Wait before redrawing (isn't strictly necessary)
+        ring[out] = Rgb(255, 0, 0); // Implicitly convert cartesian coordinates(Coords) to polar coordinates(ring[Index]) and turn on coresponding LED 
+        ring.show(); // Show changes on display
 
-        vTaskDelay(10 / portTICK_PERIOD_MS);
+        vTaskDelay(10 / portTICK_PERIOD_MS); // Wait before starting new cycle
     }
 }
 }
